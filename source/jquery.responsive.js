1ab7b4ff71ad26322367fa5388f0f73784e4afb1
Jensen Tonne <jstonne@stackideas.com>
2013-09-06 13:50:09 +0800

    Revamped with proper Responsive class. resize => applyCondition, deresize => removeCondition. targetFuncton => whenApplied, reverseFunction => whenRemoved. "at" option no longer accepts function. Only one responsive instance, previous instance gets destroyed. You should set multiple classnames instead of applying multiple conditions. Use sortBy for condition sorting.

--- source/jquery.responsive.js
+++ source/jquery.responsive.js
@@ -1,17 +1,33 @@
 // $(selector).responsive({condition});
 // $(selector).responsive([{condition1}, {condition2}]);
-$.fn.responsive = function(conditions) {
 
-	/* conditions = {
-		at: 0, // threshold value
-		switchTo: '', // classname to apply to the node
+/*var defaultOptions = {
+	// main element width to calculate
+	elementWidth: function() {}, // a function that returns pixel value
+
+	// array of conditions of ascending thresholdWidth
+	conditions: [{
+
+		// threshold for this condition
+		at: 0,// threshold value
+
+		// condition specific options
+		switchTo: '',// classname to apply to the node
 		alsoSwitch: {
 			'selector': 'class'
-		}
+		}, //  objects with element and class
 		switchStylesheet: '',
-		targetFunction: '',
-		reverseFunction: ''
-	} */
+		whenApplied: '', // function to run
+		whenRemoved: '' // reverse function that reverses any action in target function
+	}]
+}*/
+
+$.responsive = function(elem, options) {
+	
+	return new Responsive(elem, options);
+};
+
+$.fn.responsive = function(conditions) {
 
 	if (conditions) {
 		
@@ -29,234 +45,140 @@ $.fn.responsive = function(conditions) {
 	return this;
 };
 
-/*
-$.responsive({
-	elementWidth: function() {} // width calculation of the target element
-}, {
-	condition1
-});
+var $window = $(window),
+	$isFunc = $.isFunction;
 
-$.responsive({
-	elementWidth: function() {} // width calculation of the target element
-}, [{
-	condition1
-}, {
-	condition2
-}]);
-*/
+var Responsive = function(elem, options) {
 
-/*var defaultOptions = {
-	// main element width to calculate
-	elementWidth: function() {}, // a function that returns pixel value
+	var self = this,
+		elem = $(elem),
+		instance = elem.data("$responsive");
 
-	// array of conditions of ascending thresholdWidth
-	conditions: [{
+	// If there is an existing instance, kill it.
+	if (instance) instance.destroy();
 
-		// threshold for this condition
-		at: 0,
-
-		// condition specific options
-		switchTo: '',
-		alsoSwitch: {}, //  objects with element and class
-		switchStylesheet: '',
-		targetFunction: '', // function to run
-		reverseFunction: '' // reverse function that reverses any action in target function
-	}]
-}*/
-
-$.responsive = function(elem, options) {
-
-	// Make sure that single condition object gets convert into array any how
-	options.conditions = $.makeArray(options.conditions);
-
-	// Accept node, selectors, jQuery elements.
-	var elem = $(elem);
-		
-		resizeWindow = $(window),
-
-		// Debounce resize handler to ensure it won't
-		// choke up browser when window is resized.
-		resizeHandler =
-			$.debounce(function(){
-				$.responsive.update.call(elem, options);
-			}, 250),
-
-		// Get resize event namespace if it exists
-		resizeEvent = elem.data("responsiveResize") ||
-
-			// Create a new resize event namespace if it
-			// hasn't been created yet for this element.
-			(function(){
-				resizeEvent = "resize." + $.uid();
-				elem.data("responsiveResize", resizeEvent);
-				return resizeEvent;
-			})();
-
-	resizeWindow
-		// Ensure any previous resize handler is unbinded
-		.off(resizeEvent)
-		// before we bind a new resize handler with new options.
-		.on(resizeEvent, resizeHandler);
-
-	// This is debounced, if there were multiple $.fn.responsive()
-	// being called in a single chain, only the last one gets executed.
-	resizeHandler();
-};
-
-$.responsive.update = function(options) {
-
-	var node = this,
-		totalConditions = options.conditions.length;
-
-	$.responsive.sortConditions(options);
-
-	var elementWidth;
-
-	// calculate element width
-	if ($.isFunction(options.elementWidth)) {
-		elementWidth = options.elementWidth();
-	} else {
-		elementWidth = options.elementWidth;
-	}
-
-	// loop through each condition
-	$.each(options.conditions, function(i, condition) {
-		var conditionOptions = $.responsive.properConditions(condition);
-
-		var thresholdWidth = condition.at;
-
-		// calculate threshold width
-		if ($.isFunction(condition.at)) {
-			thresholdWidth = condition.at();
-		} else {
-			thresholdWidth = condition.at;
-		}
-
-		// perform resize if element <= threshold
-		if (elementWidth <= thresholdWidth) {
-
-			// remove all other condition first
-			$.responsive.resetToDefault.call(node, options.conditions, i);
-
-			// apply current condition
-			$.responsive.resize.call(node, conditionOptions);
-			return false;
-		} else {
-			$.responsive.deresize.call(node, conditionOptions);
-		}
+	// Construct instance
+	$.extend(self, {
+		// Accept node, selectors, jQuery elements.
+		elem      : elem,
+		options   : options,
+		conditions: $.sortBy($.makeArray(options.conditions), function(condition){ return condition.at; }),
+		event     : "resize.responsive" + $.uid(),
+		handler   : $.debounce(function(){ self.set(); }, 250)
 	});
+
+	// Delete conditions prop from options
+	delete options.conditions;
+
+	// Store instance within element
+	elem.data("$responsive", self)
+
+	// Attach resize handler to window
+	$window.on(self.event, self.handler);
+
+	// Set conditions
+	self.set();
 }
 
-$.responsive.resize = function(condition) {
-	var node = this;
+$.extend(Responsive.prototype, {
 
-	if (condition.switchTo) {
-		$.each(condition.switchTo, function(i, classname) {
-			node.addClass(classname);
-		});
-	}
+	set: function() {
 
-	if (condition.alsoSwitch) {
-		$.each(condition.alsoSwitch, function(selector, classname) {
-			$(selector).addClass(classname);
-		});
-	}
+		var self = this;
 
-	if (condition.targetFunction) {
-		condition.targetFunction();
-	}
+		// Remove current condition
+		self.removeCondition(self.currentCondition);
 
-	if (condition.switchStylesheet) {
-		$.each(condition.switchStylesheet, function(i, stylesheet) {
-			var tmp = $('link[href$="' + stylesheet + '"]');
-			if (tmp.length < 1) {
-				$('<link/>', {
-					rel: 'stylesheet',
-					type: 'text/css',
-					href: stylesheet
-				}).appendTo('head');
+		// Get width
+		var elementWidth = self.options.elementWidth,
+			currentWidth = ($isFunc(elementWidth)) ? elementWidth() : elementWidth;
+
+		// Analyze all conditions
+		$.each(self.conditions, function(i, condition) {
+
+			var thresholdWidth = condition.at;
+
+			if (currentWidth <= thresholdWidth) {
+				self.applyCondition(condition);
+				return false;
 			}
 		});
-	}
-};
+	},
 
-$.responsive.deresize = function(condition) {
-	var node = this;
+	applyCondition: function(condition) {
 
-	if (condition.switchTo) {
-		$.each(condition.switchTo, function(i, classname) {
-			node.removeClass(classname);
+		var switchTo, alsoSwitch, switchStylesheet, whenApplied;
+
+		// Classnames to remove
+		(switchTo = condition.switchTo) &&
+			this.elem.addClass(switchTo);
+
+		// Classnames to remove on other elements
+		(alsoSwitch = condition.alsoSwitch) &&
+			$.each(alsoSwitch, function(selector, classname) {
+				$(selector).addClass(classname);
+			});
+
+		// Stylesheets to remove
+		(switchStylesheet = condition.switchStylesheet) &&
+			$.each($.makeArray(switchStylesheet), function(i, url) {
+				// Load stylesheet if it hasn't been loaded.
+				var stylesheet = $('link[href$="' + url + '"]');
+				if (stylesheet.length < 1) {
+					$('<link/>')
+						.attr({
+							rel : 'stylesheet',
+							type: 'text/css',
+							href: url
+						})
+						.appendTo('head');
+				}
+			});
+
+		// Callback to execute when this condition is removed.
+		(whenApplied = condition.whenApplied) &&
+			$isFunc(whenApplied) && whenApplied();
+
+		this.currentCondition = condition;
+	},
+
+	removeCondition: function(condition) {
+
+		if (!condition) return;
+
+		var switchTo, alsoSwitch, switchStylesheet, whenRemoved;
+
+		// Classnames to remove
+		(switchTo = condition.switchTo) &&
+			this.elem.removeClass(switchTo);
+
+		// Classnames to remove on other elements
+		(alsoSwitch = condition.alsoSwitch) &&
+			$.each(alsoSwitch, function(selector, classname) {
+				$(selector).removeClass(classname);
+			});
+
+		// Stylesheets to remove
+		(switchStylesheet = condition.switchStylesheet) &&
+			$.each($.makeArray(switchStylesheet), function(i, url) {
+				$('link[href$="' + url + '"]').remove();
+			});
+
+		// Callback to execute when this condition is removed.
+		(whenRemoved = condition.whenRemoved) &&
+			$isFunc(whenRemoved) && whenRemoved();
+	},
+
+	resetToDefault: function(current) {
+
+		$.each(this.conditions, function(i, condition) {
+			if (current && i == current) return;
+			this.removeCondition(condition);
 		});
-	}
+	},
 
-	if (condition.alsoSwitch) {
-		$.each(condition.alsoSwitch, function(selector, classname) {
-			$(selector).removeClass(classname);
-		});
-	}
-
-	if (condition.reverseFunction) {
-		condition.reverseFunction();
-	}
-
-	if (condition.switchStylesheet) {
-		$.each(condition.switchStylesheet, function(i, stylesheet) {
-			$('link[href$="' + stylesheet + '"]').remove();
-		});
-	}
-};
-
-$.responsive.resetToDefault = function(options, current) {
-	var node = this;
-	$.each(options, function(i, condition) {
-
-		var conditionOptions = $.responsive.properConditions(condition);
-
-		if (current && i == current) {
-			return true;
-		} else {
-			$.responsive.deresize.call(node, conditionOptions);
-		}
-	});
-};
-
-$.responsive.properConditions = function(condition) {
-	var conditionOptions = {
-		at: condition.at,
-		alsoSwitch: condition.alsoSwitch,
-		switchTo: $.makeArray(condition.switchTo),
-		switchStylesheet: $.makeArray(condition.switchStylesheet),
-		targetFunction: condition.targetFunction,
-		reverseFunction: condition.reverseFunction
-	};
-
-	return conditionOptions;
-};
-
-$.responsive.sortConditions = function(options) {
-	var totalConditions = options.conditions.length;
-
-	for (var i = 0; i < totalConditions; i++) {
-		for (var j = i + 1; j < totalConditions; j++) {
-			var a, b;
-
-			if ($.isFunction(options.conditions[i].at)) {
-				a = options.conditions[i].at();
-			} else {
-				a = options.conditions[i].at;
-			}
-
-			if ($.isFunction(options.conditions[j].at)) {
-				b = options.conditions[j].at();
-			} else {
-				b = options.conditions[j].at;
-			}
-
-			if (a > b) {
-				var tmp = options.conditions[i];
-				options.conditions[i] = options.conditions[j];
-				options.conditions[j] = tmp;
-			}
-		}
-	}
-};
+	destroy: function() {
+		$window.off(this.event);
+		this.removeCondition(this.currentCondition);
+	}	
+});
